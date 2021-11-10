bb7276e8dceed3293a8851c66758c2fa035d207e
r00tkitty <r00tkittycat@gmail.com>
2021-11-10 20:26:29 +0100

    Added sites

---
+++ site/php/main.php
@@ -0,0 +1,23 @@
+<html>
+    <head>
+        <meta charset="utf-8">
+        <title>CashDash</title>
+    </head>
+    <body>
+    <div id="mySidebar" class="sidebar">
+  <a href="javascript:void(0)" class="closebtn" onclick="closeNav()">&times;</a>
+  <a href="#">Graph</a>
+  <a href="#">Main</a>
+  <a href="#">Spend/Get</a>
+  </div>
+
+<div id="main">
+  <button class="openbtn" onclick="openNav()">&#9776; Open Sidebar</button>
+  <h2>Collapsed Sidebar</h2>
+  <p>Content...</p>
+</div>
+       <?php
+      $total;
+       ?>
+    </body>
+</html>
