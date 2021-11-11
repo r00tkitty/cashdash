809314ec8b79bc9139c0980364af05006c1cf6e8
r00tkitty <r00tkittycat@gmail.com>
2021-11-11 21:52:10 +0100

    put shit in it

---
+++ site/php/start.php
@@ -0,0 +1,24 @@
+<html>
+    <head>
+        <meta charset="utf-8">
+        <title>CashDash</title>
+        <link rel="icon" href="favicon.png">
+
+	  	  <style type="text/css">
+    .deftext {
+      font-family: Helvetica, sans-serif;
+      font-size: medium;
+      text-align: center;
+      
+    }
+    .thebigfuckingvalueinthemiddle {
+     font-family: 'Franklin Gothic Medium', 'Arial Narrow', Arial, sans-serif;
+     font-size: large;
+     text-align: center;
+    }
+  </style>
+    </head>
+    <body>
+        <p class="deftext">You need to <a href="login">Log in.php</a> or <a href="register">register.php</a> to access our page</p>
+    </body>
+</html>
