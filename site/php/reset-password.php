508a640569c1b7f8cbcbd33222b8165c6e62ef7a
r00tkitty <r00tkittycat@gmail.com>
2021-11-22 14:37:01 +0100

    fixed the redirect issue in reset password page

--- site/php/reset-password.php
+++ site/php/reset-password.php
@@ -108,7 +108,7 @@ if($_SERVER["REQUEST_METHOD"] == "POST"){
             </div>
             <div class="form-group">
                 <input type="submit" class="btn btn-primary" value="Submit">
-                <a class="btn btn-link ml-2" href="welcome.php">Cancel</a>
+                <a class="btn btn-link ml-2" href="you.php">Cancel</a>
             </div>
         </form>
     </div>    
