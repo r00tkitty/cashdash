bd9bdf17eecd307503bb623630c61af33d475839
r00tkitty <r00tkittycat@gmail.com>
2022-01-13 12:10:17 +0100

    finished.

--- site/php/you.php
+++ site/php/you.php
@@ -10,7 +10,28 @@ if(!isset($_SESSION["loggedin"]) || $_SESSION["loggedin"] !== true){
 $id = $_SESSION["id"];
 $username = $_SESSION["username"];
 ?>
- 
+ <?php
+   $dbhost = 'localhost';
+   $dbuser = 'root';
+   $dbpass = '';
+   
+   $conn = new mysqli($dbhost, $dbuser, $dbpass);
+   
+   if(! $conn ) {
+      die('Could not connect: ' . $conn->error);
+   }
+   $sql = "SELECT created_at
+   FROM users
+   WHERE users.username = '$username'";
+   $conn->select_db('app');
+   $retval = $conn ->query($sql);
+   
+   if(! $retval ) {
+    die('<img src="img/error.png" style="display: block; margin-left: auto; margin-right: auto;"></img> <br><p class="error">We are having issues fetching your data.</p><br><p class="error2">Please try again later.</p>' . $conn->error);
+   }
+   $date = $retval->fetch_array()[0] ?? '';
+   $conn->close();
+?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -57,7 +78,20 @@ function closeNav() {
 <h1 class="deftext"><b>Your profile</b></h1>
 <div class="container"> 
 <p class="name"><?php echo $username;?></p>
+<p class="date"><?php echo "Account created at: <b> $date</b>";?></p>
+<button class="center"><a href="reset-password.php" class="textdeco">Reset Your Password</a></button>
+<button class="center"><a href="logout.php" class="textdeco">Log out of your account</a></button>
+<form  action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]); ?>" method="post">
+<input type='submit'  name='hehe' value="Delete your account">
+</form>
 
+<?php
+if(isset($_POST['hehe'])){
+ echo '<button><a onclick="return confirm(\'Are you sure you want to delete your Sangros ID? \nALL your data will be deleted!\')" href="deleteaccount.php">Are you sure?</a></button>';
+
+
+{}
+}?>
 
 </div>
 
